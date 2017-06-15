ed902404f24e039f83a06dbadededfa8eea17968
Skyler Kuhn <kuhnsa2@mymail.vcu.edu>
2017-06-15 16:11:14 -0400

    Create windchill.py

--- windchill.py
+++ windchill.py
@@ -1,6 +1,8 @@
+#
+# Creates a windchill table from a pre-determined range of temperatures and wind velocities
 filename = "windchill_output.txt"
 with open(filename,"w") as wind:
-    wind.write("\t\t5\t\t10\t\t15\t\t20\t\t25\t\t30\t\t35\t\t40\t\t45\t\t50")
+    wind.write("\t5\t10\t15\t20\t25\t30\t35\t40\t45\t50")
     for t in range(-20,81,10):
         wind.write("\n{}   \t".format(t))
         for v in range(5,51,5):
